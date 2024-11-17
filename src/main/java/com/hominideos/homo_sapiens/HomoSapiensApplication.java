f78c1ebfb594dcfa3c92f33e0907d29c4c1abfc5
luan medeiros <luanmedeiroscinza@gmail.com>
2024-11-17 10:20:20 -0300

    API - estrutura incial

---
+++ src/main/java/com/hominideos/homo_sapiens/HomoSapiensApplication.java
@@ -0,0 +1,13 @@
+package com.hominideos.homo_sapiens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomoSapiensApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomoSapiensApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hominideos/homo_sapiens/HomoSapiensApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hominideos.homo_sapiens;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomoSapiensApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
