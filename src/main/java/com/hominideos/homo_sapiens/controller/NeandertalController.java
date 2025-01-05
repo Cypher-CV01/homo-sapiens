9cb29be11529e3581ae3071d43b8154320163065
Leopoldo B Barreiro <leopoldo.java@gmail.com>
2025-01-05 11:01:02 -0300

    Corrigido caminho da URL específica para neandertais

--- src/main/java/com/hominideos/homo_sapiens/controller/NeandertalController.java
+++ src/main/java/com/hominideos/homo_sapiens/controller/NeandertalController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hominideos.homo_sapiens.model.Neandertal;
@@ -11,7 +12,8 @@ import com.hominideos.homo_sapiens.service.NeandertalService;
 
 // A classe anotada com @RestController do Spring permite criar 'endpoints' para a API REST
 // Cria uma URL nova para a API REST, com operações proprias implementadas na classe
-@RestController("/neandertais")
+@RestController
+@RequestMapping("/neandertais")
 public class NeandertalController {
 
 	// Carregada a classe de serviço com o @Autowired
