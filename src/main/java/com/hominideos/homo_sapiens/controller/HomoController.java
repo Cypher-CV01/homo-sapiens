0a37ac703d9e5102e936de3b1d59d4f849da162e
Leopoldo B Barreiro <leopoldo.java@gmail.com>
2025-01-05 10:52:55 -0300

    Alterado o verbo HTTP para respeitar o padrão REST

--- src/main/java/com/hominideos/homo_sapiens/controller/HomoController.java
+++ src/main/java/com/hominideos/homo_sapiens/controller/HomoController.java
@@ -1,7 +1,7 @@
 package com.hominideos.homo_sapiens.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 //import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,16 +10,15 @@ import com.hominideos.homo_sapiens.model.Neandertal;
 import com.hominideos.homo_sapiens.service.HomoService;
 
 @RestController
-@RequestMapping("/homo")
+@RequestMapping("/homos")
 public class HomoController {
 
 	@Autowired
 	private HomoService services;
 
-	@GetMapping("/new")
-	Neandertal criarNeandertal() {
+	@PostMapping("/new")
+	public Neandertal criarNeandertal() {
 		Neandertal florence = services.criarNeandertal(1400.0f, 80.0f, 83.0f, 28.5f, "tipo de primata de cabeça grande");
 		return florence;
-
 	}
 }
