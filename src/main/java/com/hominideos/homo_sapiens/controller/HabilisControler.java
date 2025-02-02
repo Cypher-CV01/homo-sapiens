e487a0e934d2730f8d17d8c33246b513a443977a
Luan Medeiros <luanmedeiroscinza@gmail.com>
2025-02-02 11:44:32 -0300

    CRUD do homo Habilis

---
+++ src/main/java/com/hominideos/homo_sapiens/controller/HabilisControler.java
@@ -0,0 +1,60 @@
+package com.hominideos.homo_sapiens.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hominideos.homo_sapiens.model.FerramentaHabilis;
+import com.hominideos.homo_sapiens.model.Habilis;
+import com.hominideos.homo_sapiens.service.HabilisService;
+
+
+@RestController
+@RequestMapping("/habilis")
+public class HabilisControler {
+	
+	@Autowired
+	private HabilisService service;
+	
+	@GetMapping("/buscar-habilis")
+	public List<Habilis > buscarHabilis() {
+		return service.buscarHabilis(); 
+	}
+	
+	@PostMapping("/novo-habilis")
+	public Habilis criarHabilis(@RequestParam String nome, @RequestParam String sobrenome, @RequestParam FerramentaHabilis habilidade) {
+	
+		return service.criarHabilis(nome, sobrenome, habilidade);
+	}
+	
+	@PutMapping("/atualizar-habilis")
+	public Habilis atualizarNeandertal(@RequestBody Habilis habilis) {
+		
+		return service.atualizarHabilis(habilis);
+	}
+	
+	@DeleteMapping("/deletar-habilis")
+	public ResponseEntity<String> deletarHabilis(@RequestParam Long id){
+		try {
+			service.deletarHabilis(id);
+			return new ResponseEntity<String>("Habilis deletado com sucesso", HttpStatus.OK);
+		} catch (Exception e) {
+			return new ResponseEntity<String>(e.getMessage(), HttpStatus.NOT_FOUND);
+		}
+		
+	}
+	
+
+	
+
+}
---
+++ src/main/java/com/hominideos/homo_sapiens/service/HabilisService.java
@@ -0,0 +1,56 @@
+package com.hominideos.homo_sapiens.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hominideos.homo_sapiens.model.FerramentaHabilis;
+import com.hominideos.homo_sapiens.model.Habilis;
+import com.hominideos.homo_sapiens.repository.HabilisRepository;
+
+@Service
+public class HabilisService {
+
+	@Autowired
+	private HabilisRepository repository;
+	
+	public List<Habilis> buscarHabilis(){
+		return repository.findAll();
+	}
+		
+	public Habilis criarHabilis(String nome, String sobrenome, FerramentaHabilis habilidade) {
+		var habilis = new Habilis();
+		habilis.setNome(nome);
+		habilis.setSobrenome(sobrenome);
+		habilis.setHabilidade(habilidade);
+		
+		return repository.save(habilis);		
+	} 
+	
+	public Habilis atualizarHabilis(Habilis individuo) {
+		var habilisDoBanco = repository.findOneById(individuo.getId());
+		habilisDoBanco.setBreveDescring(individuo.getBreveDescring());
+		habilisDoBanco.setCmBraco(individuo.getCmBraco());
+		habilisDoBanco.setCmCubicoCranio(individuo.getCmCubicoCranio());
+		habilisDoBanco.setCmPe(individuo.getCmPe());
+		habilisDoBanco.setNome(individuo.getNome());
+		habilisDoBanco.setPeso(individuo.getPeso());
+		habilisDoBanco.setPostura(individuo.getPostura());
+		habilisDoBanco.setSobrenome(individuo.getSobrenome());
+		habilisDoBanco.setTamanhoGrupo(individuo.getTamanhoGrupo());
+		habilisDoBanco.setHabilidade(individuo.getHabilidade());
+		return repository.save(habilisDoBanco);		
+		
+	}
+	
+	public void deletarHabilis(Long id) {
+		var habilisNoBanco = repository.findOneById(id);
+		if (habilisNoBanco == null) {
+			throw new RuntimeException("Habilis inexistente nos registros.");
+		} else {
+			repository.deleteById(id);
+		}
+	}
+	
+}
