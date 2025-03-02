86fb2de73081ba4ab27486656ed723a22fa11eeb
Luan Medeiros <luanmedeiroscinza@gmail.com>
2025-03-02 12:45:25 -0300

    implementação CRUD-Florisience

---
+++ src/main/java/com/hominideos/homo_sapiens/model/FerramentaFlorisience.java
@@ -0,0 +1,9 @@
+package com.hominideos.homo_sapiens.model;
+
+public enum FerramentaFlorisience {
+	LASCAS_DE_PEDRA,
+	BIFACES_SIMPLES,
+	RASPADORES,
+	PONTAS_DE_lANÇA,
+	CORTADORES_DE_CARNE;
+}
---
+++ src/main/java/com/hominideos/homo_sapiens/model/Florisience.java
@@ -0,0 +1,20 @@
+package com.hominideos.homo_sapiens.model;
+
+import jakarta.persistence.DiscriminatorValue;
+import jakarta.persistence.Entity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Entity
+@DiscriminatorValue("Florisience")
+@ToString(callSuper = true)
+@Getter
+@Setter
+
+public class Florisience extends Homo{
+
+	private Float altura;
+	private FerramentaFlorisience ferramenta;
+	
+}
