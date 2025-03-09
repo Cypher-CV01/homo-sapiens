59f82bd6a9efb834bb02793b723ccbf9caf07aeb
Leopoldo B Barreiro <leopoldo.java@gmail.com>
2025-03-09 11:38:05 -0300

    Removida linha em branco

--- src/main/java/com/hominideos/homo_sapiens/config/RabbitConfig.java
+++ src/main/java/com/hominideos/homo_sapiens/config/RabbitConfig.java
@@ -6,10 +6,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-// Esta classe configura e que habilita o RabbitMQ na API
 @Configuration
 @EnableRabbit
-
 public class RabbitConfig {
 
 	@Value("${amqp.neandertal.producer-queue}")
