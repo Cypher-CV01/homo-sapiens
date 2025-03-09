82042245b3c6c1802a9fe2a384d3cd70f1cbb7de
Leopoldo B Barreiro <leopoldo.java@gmail.com>
2025-03-09 11:32:33 -0300

    Correções no PR

--- src/main/java/com/hominideos/homo_sapiens/amqp/QueueConsumer.java
+++ src/main/java/com/hominideos/homo_sapiens/amqp/QueueConsumer.java
@@ -3,7 +3,6 @@ package com.hominideos.homo_sapiens.amqp;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
-
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
