c8dcde1422a183b5793c50d2aed440963bb821ee
Leopoldo B Barreiro <leopoldo.java@gmail.com>
2025-06-15 10:24:24 -0300

    Definida base de classes para varredura de configuracoes do mongo

--- src/main/java/com/hominideos/homo_sapiens/config/MongoConfig.java
+++ src/main/java/com/hominideos/homo_sapiens/config/MongoConfig.java
@@ -3,9 +3,11 @@ package com.hominideos.homo_sapiens.config;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
+import com.hominideos.homo_sapiens.aux.Vegetal;
+import com.hominideos.homo_sapiens.auxrepository.VegetalRepository;
+
 @Configuration
-@EnableMongoRepositories
+@EnableMongoRepositories(basePackageClasses = { Vegetal.class, VegetalRepository.class })
 public class MongoConfig {
 
-	
 }
