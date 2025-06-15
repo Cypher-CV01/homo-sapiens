a2c2c08b5822edcc51b2c48e83ba57343925aee7
Leopoldo B Barreiro <leopoldo.java@gmail.com>
2025-06-15 09:51:08 -0300

    Utilizando annotations do lombok

--- src/main/java/com/hominideos/homo_sapiens/aux/TipoCaule.java
+++ src/main/java/com/hominideos/homo_sapiens/aux/TipoCaule.java
@@ -1,32 +1,16 @@
 package com.hominideos.homo_sapiens.aux;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum TipoCaule {
 	AEREO("É caule do tipo aéreo", 1),
 	SUBTERRANEO("É caule do tipo subterrâneo", 2),
 	AQUATICO("É caule do tipo aquático", 3);
-	
-	private int ordem;	
-	private String descricao;
 
-	TipoCaule(String descricao, int ordem) {
-		this.descricao = descricao;
-		this.ordem = ordem;
-	}	
-	
-	public int getOrdem() {
-		return ordem;
-	}
-	
-	public void setOrdem(int ordem) {
-		this.ordem = ordem;
-	}
-	
-	public String getDescricao() {
-		return descricao;
-	}
-	
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
-	}
+	private String descricao;
+	private int ordem;
 
 }
--- src/main/java/com/hominideos/homo_sapiens/auxrepository/VegetalRepository.java
+++ src/main/java/com/hominideos/homo_sapiens/auxrepository/VegetalRepository.java
@@ -1,9 +1,11 @@
 package com.hominideos.homo_sapiens.auxrepository;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
 
 import com.hominideos.homo_sapiens.aux.Vegetal;
 
+@Repository
 public interface VegetalRepository extends MongoRepository<Vegetal, String> {
 
 }
